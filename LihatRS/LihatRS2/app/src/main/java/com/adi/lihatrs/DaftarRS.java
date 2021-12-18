353328c91a1450d85a17b11a7caaaaba379755de
adirhmn <56228999+adirhmn@users.noreply.github.com>
2021-12-18 12:43:42 +0700

    Add files via upload

---
+++ LihatRS/LihatRS2/app/src/main/java/com/adi/lihatrs/DaftarRS.java
@@ -0,0 +1,14 @@
+package com.adi.lihatrs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaftarRS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_rs);
+    }
+}
---
+++ LihatRS/LihatRS2/app/src/main/java/com/adi/lihatrs/PilihProvinsi.java
@@ -0,0 +1,14 @@
+package com.adi.lihatrs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PilihProvinsi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_provinsi);
+    }
+}
